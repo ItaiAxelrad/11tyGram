8f53e130fca76f66bd0411549ce358b74c4f87bc
ItaiAxelrad <itaiaxelrad@gmail.com>
2020-06-20 17:19:22 -0600

    add data

---
+++ .eleventy.js
@@ -0,0 +1,44 @@
+const { DateTime } = require('luxon');
+
+module.exports = function(eleventyConfig) {
+  // set deep merge to true
+  eleventyConfig.setDataDeepMerge(true);
+  // pass media through
+  eleventyConfig.addPassthroughCopy("icon.png");
+  eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("photos");
+  eleventyConfig.addPassthroughCopy("videos");
+
+  // parse datetime to readable
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromISO(dateObj, { zone: 'utc' }).toLocaleString(
+      DateTime.DATE_FULL
+    );
+  });
+
+  // parse datetime to html
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromISO(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+
+    // These are all optional, defaults are shown:
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "JSON",
+      output: "_site"
+    }
+  };
+};
