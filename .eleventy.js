01e8169c3fb4218c0583a3a1c754b455c5177f93
ItaiAxelrad <itaiaxelrad@gmail.com>
2022-10-28 11:01:36 -0600

    Remove unused files

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,11 @@
-const { DateTime } = require('luxon');
-const markdownIt = require('markdown-it');
-const hashtag = require('markdown-it-hashtag');
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
 
   // add plugin
   eleventyConfig.addPlugin(pluginRss);
 
-  // set deep merge to true
-  eleventyConfig.setDataDeepMerge(true);
   // pass media through
   eleventyConfig.addPassthroughCopy('images');
   eleventyConfig.addPassthroughCopy('styles');
@@ -29,16 +25,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromISO(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
   });
-
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true,
-    typographer: false,
-  }).use(hashtag);
-  
-  eleventyConfig.setLibrary('md', markdownLibrary);
 
   return {
     templateFormats: ['md', 'njk', 'html', 'liquid'],
--- README.md
+++ README.md
@@ -11,14 +11,15 @@ Download a copy of your data from Instagram on the Web:
 - [ ] Go to your profile.
 - [ ] Click Privacy and Security.
 - [ ] Scroll down to Data Download and click Request Download.
-- [ ] Enter the email address where you'd like to receive a link to your data and click Next.
+- [ ] Enter the email address where you'd like to receive a link to your data.
+- [ ] Select 'JSON' under Information format and click Next.
 - [ ] Enter your Instagram account password and click Request Download.
 - [ ] You'll soon receive an email titled Your Instagram Data with a link to your data.
 - [ ] Click Download Data and follow the instructions to finish downloading your information.
 
 ## Add Your Data
 
-Drop the downloaded data into the `src` directory.
+Drop the downloaded data into the `src` directory and remove any sensitive information.
 
 ## Create your 11tyGram
 
